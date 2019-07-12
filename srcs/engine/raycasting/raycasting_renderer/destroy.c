4c46182fc35796a4432ea3ca3c66db5e9ca8d550
lbenard <lbenard@student.42.fr>
2019-07-13 00:08:38 +0200

    destroy refact for raycasting renderer

--- srcs/engine/raycasting/raycasting_renderer/destroy.c
+++ srcs/engine/raycasting/raycasting_renderer/destroy.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free.c                                             :+:      :+:    :+:   */
+/*   destroy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 16:20:20 by lbenard           #+#    #+#             */
-/*   Updated: 2019/05/12 16:20:55 by lbenard          ###   ########.fr       */
+/*   Updated: 2019/07/13 00:06:05 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "engine/raycasting.h"
 
-void	free_raycasting_renderer(t_raycasting_renderer *const self)
+void	destroy_raycasting_renderer(t_raycasting_renderer *const self)
 {
 	free(self->columns);
 	free(self);
