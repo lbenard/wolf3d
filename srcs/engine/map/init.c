89868ffc6065f7f907831b52fabc5dc1db9faa5e
lbenard <lbenard@student.42.fr>
2019-10-04 12:53:55 +0200

    fixed map init leak

--- srcs/engine/map/init.c
+++ srcs/engine/map/init.c
@@ -6,7 +6,7 @@
 /*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 16:45:58 by lbenard           #+#    #+#             */
-/*   Updated: 2019/10/03 18:14:01 by lbenard          ###   ########.fr       */
+/*   Updated: 2019/10/04 12:53:41 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,28 @@
 #include "engine/error.h"
 #include "engine/parsing.h"
 
-t_result	init_map_split(t_map *const self,
-				char *map_file_str)
+t_result	init_map_split(t_map *const self, char *map_file_str)
 {
 	if (!map_parse_size(self, wolf_select_flag(map_file_str, "size")))
 	{
 		free(map_file_str);
+		free_texture_list(&self->textures);
+		free_block_list(&self->blocks);
 		return (throw_result_str("init_map()", "failed to parse size"));
 	}
 	if (!map_parse_map(self, wolf_select_flag(map_file_str, "map")))
 	{
 		free(map_file_str);
+		free_texture_list(&self->textures);
+		free_block_list(&self->blocks);
 		return (throw_result_str("init_map()", "failed to parse map"));
 	}
 	if (!map_parse_player(self, wolf_select_flag(map_file_str, "player")))
 	{
 		free(map_file_str);
+		free_texture_list(&self->textures);
+		free_block_list(&self->blocks);
+		free(self->map);
 		return (throw_result_str("init_map()", "failed to parse player"));
 	}
 	return (OK);
@@ -57,6 +63,7 @@ t_result	init_map(t_map *const self, const t_map_args *const args)
 	if (!map_parse_block_list(self, wolf_select_flag(map_file_str, "blocks")))
 	{
 		free(map_file_str);
+		free_texture_list(&self->textures);
 		return (throw_result_str("init_map()", "failed to parse blocks"));
 	}
 	if (init_map_split(self, map_file_str) == ERROR)
